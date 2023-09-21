9d8f1c06066a793f04b9e6fc624a843fbfeaccdf
Mathias Munk <mh@morningtrain.dk>
2023-09-21 17:58:18 +0200

    Rewrite

--- src/Classes/Action.php
+++ src/Classes/Action.php
@@ -2,84 +2,21 @@
 
 namespace Morningtrain\WP\Hooks\Classes;
 
+use Attribute;
+
 /**
  * A WordPress action
  *
  * @see https://developer.wordpress.org/reference/functions/add_action/
  */
+#[Attribute(Attribute::IS_REPEATABLE | Attribute::TARGET_METHOD)]
 class Action extends \Morningtrain\WP\Hooks\Abstracts\AbstractHook
 {
 
-    protected bool $proactive = false;
-
-    /**
-     * Set the method for handling the action call
-     *
-     * This is the same as the callback that you may supply as a second param in the constructor
-     *
-     * @param  string|callable  $callback
-     * @return $this
-     */
-    public function handle(string|callable $callback): static
+    public function register(callable $callback, int $numArgs)
     {
-        $this->callback = $callback;
 
-        return $this;
-    }
+        \add_action($this->hook, $callback, $this->priority, $numArgs);
 
-    /**
-     * Render a view as the action callback
-     *
-     * @param  string  $view
-     * @return $this
-     */
-    public function view(string $view): static
-    {
-        $this->useCallbackManager('view', $view);
-        $this->numArgs = 1;
-
-        return $this;
-    }
-
-    /**
-     * Makes the action proactive
-     *
-     * This calls the callback immediately if the action has already been called.
-     *
-     * NOTE: Since the callback will be called directly, no args will be supplied.
-     * This method is mainly useful for initializing parts of the codebase after a given action has been triggered.
-     *
-     * @return $this
-     */
-    public function proactive(): static
-    {
-        $this->proactive = true;
-
-        return $this;
-    }
-
-    /**
-     * Add the action for each hook
-     * This is done on __destruct automatically
-     *
-     * @return mixed|void
-     *
-     * @throws \ReflectionException
-     */
-    protected function add()
-    {
-        if ($this->numArgs === null) {
-            $this->numArgs = $this->findNumArgs($this->callback);
-        }
-        if(is_string($this->callback) && class_exists($this->callback)){
-            $this->useCallbackManager('invoke', $this->callback);
-        }
-        foreach ((array) $this->hook as $hook) {
-            if ($this->proactive && \did_action($hook)) {
-                ($this->callback)();
-                continue;
-            }
-            \add_action($hook, $this->callback, $this->priority, $this->numArgs);
-        }
     }
 }
